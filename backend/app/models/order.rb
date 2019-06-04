757f5d19661bbe9601319a7c5e0fea81c29c94a9
devcrocodile <crazynet190@outlook.com>
2019-06-04 14:47:09 -0300

    Include validations

--- backend/app/models/order.rb
+++ backend/app/models/order.rb
@@ -1,4 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :restaurant
   has_many :Order_products
+
+  validates :name, presence: true
+  validates :phone_number, presence: true
+  validates :total_value, presence: true
+
+  enum status: {waiting: 0, delivered: 1}
 end
--- backend/app/models/product.rb
+++ backend/app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :product_category
   has_many :order_products
+
+  validates_associated :product_category
+  validates :price, presence: true
+  validates :name, presence: true
 end
