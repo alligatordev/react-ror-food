d6310fad8d9fb6a90e7da07699107261ea3efa1e
devcrocodile <crazynet190@outlook.com>
2019-06-11 11:28:47 -0300

    tests and fix API

--- backend/app/controllers/orders_controller.rb
+++ backend/app/controllers/orders_controller.rb
@@ -20,8 +20,6 @@ class OrdersController < ApplicationController
     end
     
     def order_params
-      params.require(:order).permit(
-        :name, :phone_number, :restaurant_id,
-        order_products_attributes: %i[quantity comment product_id]
-      )
+      params.require(:order).permit(:name, :phone_number, :restaurant_id, order_products_attributes: %i[quantity comment product_id])
+    end
 end
