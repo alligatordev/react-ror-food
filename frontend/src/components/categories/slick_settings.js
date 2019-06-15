5ad44ad94087eb1a93d2046c3aebbc260c504f0e
devcrocodile <crazynet190@outlook.com>
2019-06-15 16:55:31 -0300

    Categories Slider

---
+++ frontend/src/components/categories/slick_settings.js
@@ -0,0 +1,31 @@
+export default {
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 4,
+    intialSlide: 0,
+    dots: false,
+    infinite: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+};
