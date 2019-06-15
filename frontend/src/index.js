929c589270dd8f26d96f37268ab4849893a20654
devcrocodile <crazynet190@outlook.com>
2019-06-15 17:36:02 -0300

    Add react-redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from "react-redux";
+import Store from "./store"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={Store}>
+        <App />
+    </Provider>,
+document.getElementById('root'));
+
 serviceWorker.unregister();
