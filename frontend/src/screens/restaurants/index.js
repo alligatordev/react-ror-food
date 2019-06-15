765518466ac20a3b5595813910a9cf1901f2025e
alligatordev <xavierb2531@gmail.com>
2019-06-15 16:55:31 -0300

    Categories Slider

--- frontend/src/screens/restaurants/index.js
+++ frontend/src/screens/restaurants/index.js
@@ -1,8 +1,10 @@
 import React, { Fragment } from 'react'
 import ListRestaurants from "../../components/list_restaurants/index";
+import Categories from "../../components/categories"
 
 const Restaurants = () => (
     <Fragment>
+        <Categories />
         <ListRestaurants />
     </Fragment>
 )
